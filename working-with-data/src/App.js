90479fb8f584b07680bd6f6eb64af8c271bec0f8
Ignatius Seto <ignatius.seto@accenture.com>
2017-07-08 23:04:08 +0200

    working with HTTPS request

--- working-with-data/src/App.js
+++ working-with-data/src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import {Component} from 'react';
-
-let data = [
-    {   
-        name: "Ignatius Seto",
-        avatar_url: "https://avatars3.githubusercontent.com/u/17163682?v=3",
-        company: "Accenture" 
-    },
-    {
-        name: "Philippe Dijon",
-        avatar_url: "https://avatars3.githubusercontent.com/u/19623192?v=3",
-        company: "Accenture"
-    },
-]
+import axios from 'axios';
 
 class App extends Component {
+    state = {
+        cards: [
+            {   
+                name: "Ignatius Seto",
+                avatar_url: "https://avatars3.githubusercontent.com/u/17163682?v=3",
+                company: "Accenture" 
+            },
+            {
+                name: "Philippe Dijon",
+                avatar_url: "https://avatars3.githubusercontent.com/u/19623192?v=3",
+                company: "Accenture"
+            },
+        ]
+    }
+
+    addNewCard = (cardInfo) => {
+        console.log(cardInfo);
+    }
     render(){
         return(
             <div>
-                <CardList cards={data}/>
+                <Form />
+                <CardList cards={this.state.cards}/>
             </div>
         )
     }
@@ -43,6 +50,32 @@ const CardList = (props) => {
             {props.cards.map(card => <Card {...card}/>)}
         </div>
     )
+}
+
+// A form to handle user search input
+class Form extends Component {
+    state = { userName : '' }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Event: form submit', this.state.userName);
+        axios.get('https://www.github.com/users/${this.state.userName}')
+            .then(resp => {
+                console.log(resp);
+            });
+    }
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <input type="text"
+                /*alternative to using ref is 'state'*/
+                /*ref={(input) => this.userNameInput = input}*/
+                value={this.state.userName}
+                onChange = {(event) => this.setState({ userName: event.target.value })}
+                placeholder="Github username" />
+                <button type="submit">Add card</button>
+            </form>
+        )
+    }
 }
 
 export default App;

16d0f5c72e8056248ed484b24e3541fe61d8aa3d
ignatiusseto <me@ignatiusseto.com>
2017-07-10 15:26:36 +0200

    add custom CSS

--- building-game-ui/src/App.js
+++ building-game-ui/src/App.js
@@ -1,19 +1,68 @@
 import React from 'react';
 import {Component} from 'react';
+import AppCSS from './App.css';
 
 class App extends Component {
     render(){
         return(
             <div>
                 <Game />
-                <Game />
             </div>
         )
     }
 }
 
+const Stars = (props) => {
+    return (
+        <div className="col-5">
+            <i className="fa fa-star"></i>
+        </div>
+    );
+}
+
+const Button = (props) => {
+    return (
+        <div className="col-2">
+            <button>=</button>
+        </div>
+    );
+}
+
+const Answer = (props) => {
+    return (
+        <div className="col-5">
+            ...
+        </div>
+    );
+}
+
+const Numbers = (props) => {
+    return(
+        <div className="card text-center">
+            <div>
+                <span>1</span>
+                <span className="selected">2</span>
+                <span className="used">3</span>
+            </div>
+        </div>
+    )
+}
+
 class Game extends Component {
     render(){
+        return(
+            <div className="container">
+                <h3>Play Nine</h3>
+                <hr />
+                <div className="row">
+                    <Stars />
+                    <Button />
+                    <Answer />
+                </div>
+                <br />
+                <Numbers />
+            </div>
+        )
     }
 }
 

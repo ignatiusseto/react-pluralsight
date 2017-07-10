84d1ee68a5dacc4f9d3b349d337f90a1e48936e4
ignatiusseto <me@ignatiusseto.com>
2017-07-10 14:38:04 +0200

    initialize building-game-ui

---
+++ building-game-ui/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Component} from 'react';
+
+class App extends Component {
+    render(){
+        return(
+            <div>
+                <Game />
+                <Game />
+            </div>
+        )
+    }
+}
+
+class Game extends Component {
+    render(){
+    }
+}
+
+export default App;

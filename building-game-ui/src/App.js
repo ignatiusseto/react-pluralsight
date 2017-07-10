b3d6395295008dadff0ca6051b95f0a6994feb8a
ignatiusseto <me@ignatiusseto.com>
2017-07-10 16:57:42 +0200

    selecting a number

--- building-game-ui/src/App.js
+++ building-game-ui/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 }
 
 const Stars = (props) => {
-    const numberOfStars = 1 + Math.floor(Math.random()*9);
+    //const numberOfStars = 1 + Math.floor(Math.random()*9);
 
     // let stars = [];
     
@@ -24,7 +24,7 @@ const Stars = (props) => {
 
     return (
         <div className="col-5">
-            {_.range(numberOfStars).map(i =>
+            {_.range(props.numberOfStars).map(i =>
                 <i key={i} className="fa fa-star"></i>
             )}
         </div>
@@ -42,19 +42,28 @@ const Button = (props) => {
 const Answer = (props) => {
     return (
         <div className="col-5">
-            ...
+            {props.selectedNumbers.map((number, i) =>
+                <span key={i}>{number}</span>
+            )}
         </div>
     );
 }
 
 const Numbers = (props) => {
-    // const arrayOfNumbers = _.range(1,10);
+    const numberClassName = (number) => {
+        if(props.selectedNumbers.indexOf(number) >=0) {
+            return 'selected';
+        }
+    }
 
     return(
         <div className="card text-center">
             <div>
                 {Numbers.list.map((number, i) =>
-                    <span key={i}>{number}</span>
+                    <span key={i} className={numberClassName(number)}
+                                  onClick={() => props.selectNumber(number)}>
+                        {number}
+                    </span>
                 )}
             </div>
         </div>
@@ -64,18 +73,31 @@ const Numbers = (props) => {
 Numbers.list = _.range(1,10);
 
 class Game extends Component {
+    state = {
+        selectedNumbers: [],
+        randomNumberOfStars: 1 + Math.floor(Math.random()*9),
+    };
+
+    selectNumber = (clickedNumber) => {
+        if(this.state.selectedNumbers.indexOf(clickedNumber) >=0) {return;}
+        this.setState(prevState => ({
+            selectedNumbers: prevState.selectedNumbers.concat(clickedNumber)
+        }));
+    };
+
     render(){
         return(
             <div className="container">
                 <h3>Play Nine</h3>
                 <hr />
                 <div className="row">
-                    <Stars />
+                    <Stars numberOfStars={this.state.randomNumberOfStars} />
                     <Button />
-                    <Answer />
+                    <Answer selectedNumbers={this.state.selectedNumbers} />
                 </div>
                 <br />
-                <Numbers />
+                <Numbers selectedNumbers={this.state.selectedNumbers}
+                         selectNumber={this.selectNumber} />
             </div>
         )
     }
